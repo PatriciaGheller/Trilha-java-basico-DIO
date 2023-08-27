29fe5c15e622a8bcc4b43600f4246596b38cc1de
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-08-27 20:28:49 -0300

    variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+  public static void main(String[] args) throws Exception {
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 1;
+
+    numero = 2;
+
+    //valor_nunca_podera_mudar?
+    //Vamos ver variaveis x constantes
+ }
+}
