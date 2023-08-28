5525edf713f1de8d44e4cf0ded407fa024b833bf
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-08-27 22:03:47 -0300

    Operador boleano

---
+++ boleanos/src/Boleanos.java
@@ -0,0 +1,18 @@
+public class Boleanos {
+    public static void main(String[] args) throws Exception {
+        boolean variavel = true;
+
+        //negacao
+
+        System.out.println(!variavel);
+
+        /* A variavel sera false apenas nessa impressao
+         * Para que se torne negativa deve se atribuir a ela um novo valor
+         * 
+         * variavel = !variavel
+         * 
+         * essa variavel deixou de ser true e passou a ser false
+         * System.out.println(variavel);
+         */ 
+        }
+}
