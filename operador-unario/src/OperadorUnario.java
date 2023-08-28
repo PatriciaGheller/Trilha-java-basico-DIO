947ea9d1c161d920245bd6ca253f9c5b6ec73914
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-08-27 21:25:32 -0300

    numero negativo e numero positivo

---
+++ operador-unario/src/OperadorUnario.java
@@ -0,0 +1,16 @@
+ public class OperadorUnario {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero 
+
+        System.out.println(numero);
+
+        //Tornar numero positivo:
+        numero = + numero*-1;
+
+        System.out.println(numero);
+
+
+    }
+}
