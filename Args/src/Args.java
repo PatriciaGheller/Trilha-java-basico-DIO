027226d899e5427632cb6438a64e3baedcfba9d2
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-08-30 22:16:55 -0300

    Args

---
+++ Args/src/Args.java
@@ -0,0 +1,10 @@
+  public class Args{
+
+      public static void public static void main(String[] args) {
+        
+      String nome = args [0];
+      String sobrenome = args [1];
+      int idade = Integer.valueOf(args[2]);
+      double altura = Double.valueOf(args[3]);
+      }
+    }
---
+++ Argumentos/src/Args.java
@@ -0,0 +1,12 @@
+    public class Args{
+
+      public static void public static void main(String[] args) {
+        
+      String nome = args [0];
+      String sobrenome = args [1];
+      int idade = Integer.valueOf(args[2]);
+      double altura = Double.valueOf(args[3]);
+      }
+    }
+ 
+    
