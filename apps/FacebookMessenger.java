6ec1256482adffa8e4287d2f3827a3cd1a4e86b0
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-09-14 20:43:20 -0300

    Polimorfismo
    serviço de mensagens instantâneas

---
+++ apps/FacebookMessenger.java
@@ -0,0 +1,12 @@
+package apps;
+
+public class FacebookMessenger extends ServicoMensagemInstantanea {
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem pelo FacebookMessenger");
+}
+	
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo MSN Messenger");
+		
+	}
+}
